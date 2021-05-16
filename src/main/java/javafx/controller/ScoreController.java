4c7e12d61efea38f59bc6e28eb900a5d703f7da7
Tóth Antal <tothantal0326@gmail.com>
2021-05-16 15:48:06 +0200

    Implemented the ScoreController

---
+++ src/main/java/javafx/controller/ScoreController.java
@@ -0,0 +1,27 @@
+package javafx.controller;
+
+import java.io.IOException;
+
+import javax.xml.bind.JAXBException;
+
+import boardgame.model.Highscore;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ScoreController {
+
+	Highscore scores = new Highscore();
+	
+	
+	@FXML
+	Label scoreText;
+	
+	@FXML
+	private void initialize() throws IOException, JAXBException {
+		
+		this.scores.fromXML();
+		
+		this.scoreText.setText(scores.toString());
+	}
+	
+}
