4b0269fca679720199fdd4df1e1a52a9eadf6ddf
Tóth Antal <tothantal0326@gmail.com>
2021-05-18 20:01:31 +0200

    Score now displays even if there is no XML file

--- src/main/java/javafx/controller/ScoreController.java
+++ src/main/java/javafx/controller/ScoreController.java
@@ -1,5 +1,6 @@
 package javafx.controller;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.xml.bind.JAXBException;
@@ -11,17 +12,23 @@ import javafx.scene.control.Label;
 public class ScoreController {
 
 	Highscore scores = new Highscore();
-	
-	
+
+
 	@FXML
 	Label scoreText;
-	
+
 	@FXML
 	private void initialize() throws IOException, JAXBException {
-		
+
+		File XMLFile = new File("score.xml");
+		if (!XMLFile.exists()) {
+			XMLFile.createNewFile();
+			this.scoreText.setText("No scores yet");
+		}
+
 		this.scores.fromXML();
-		
 		this.scoreText.setText(scores.toString());
+
 	}
-	
+
 }
